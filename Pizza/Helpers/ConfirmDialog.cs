cc5861241dcb54a8ea0ebcfdb8c310d86acd6c86
Egor Belyakov <46113796+egospegos@users.noreply.github.com>
2018-12-25 07:45:58 +0300

    Add files via upload

---
+++ Pizza/Helpers/ConfirmDialog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Pizza.Helpers
+{
+    public static class ConfirmDialog
+    {
+        public static bool Confirm(string text)
+        {
+            MessageBoxResult result = MessageBox.Show(text, "Подтверждение", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+            if (result == MessageBoxResult.OK) return true;
+            else return false;
+        }
+    }
+
+    public static class DialogCloser
+    {
+        public static readonly DependencyProperty DialogResultProperty =
+            DependencyProperty.RegisterAttached(
+                "DialogResult",
+                typeof(bool?),
+                typeof(DialogCloser),
+                new PropertyMetadata(DialogResultChanged));
+
+        private static void DialogResultChanged(
+            DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            var window = d as Window;
+            if (window != null)
+                window.DialogResult = e.NewValue as bool?;
+        }
+        public static void SetDialogResult(Window target, bool? value)
+        {
+            target.SetValue(DialogResultProperty, value);
+        }
+    }
+}
